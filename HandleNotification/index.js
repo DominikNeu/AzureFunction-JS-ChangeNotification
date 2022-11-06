eb51ecdf39eb926149fc23c44493d6be49df8336
DominikNeu <dominik.neubauer@edu.fh-joanneum.at>
2022-11-06 21:22:27 +0100

    Added change notification handling

--- HandleNotification/index.js
+++ HandleNotification/index.js
@@ -1,14 +1,44 @@
 const azure = require("azure-storage");
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+    // handle change notification registration
+    if (req.query.validationToken) {
+        context.res = {
+            status: 200,
+            body: req.query.validationToken,
+            headers: {
+                "Content-Type": "text/plain",
+            },
+        };
+    // handle the change notification message
+    } else {
+        // insert AZURE_STORAGE_CONNECTION_STRING
+        context.log(req.body);
+        const tableSvc = azure.createTableService("AZURE_STORAGE_CONNECTION_STRING");
+        const link = {
+            PartitionKey: "User",
+            RowKey: String(Date.now()),//req.body.value[0].id,
+            changeType: req.body.value[0].changeType,
+            resource: req.body.value[0].resource,
+        };
+        tableSvc.insertEntity(
+            "UserChangeNotifications",
+            link,
+            function (error, result, response) {
+                if (!error) {
+                    context.res = {
+                        status: 200,
+                        body: result,
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                    };
+                }
+                    context.res = {
+                        status: 500,
+                        body: error,
+                    };
+                }
+        )
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
-    };
+    }
 }
